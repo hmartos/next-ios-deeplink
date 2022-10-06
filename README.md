7d93c08ac3cb17686d905b03fd79202d626fef35
hmartos <hectormartosg@gmail.com>
2022-10-06 19:31:06 +0200

    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# next-ios-deeplink
+
+This project is a simple Next.js project to redirect an URL to an iOS deeplink for Binance App.
+
+## Deep linking
+
+URL `/api/ios-deeplink?pair=VALUE` redirects to `bnc://app.binance.com/trade/trade?at=spot&symbol=VALUE`.
--- pages/api/ios-deeplink.js
+++ pages/api/ios-deeplink.js
@@ -1,12 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  // Open Chrome DevTools to step through the debugger!
-  // debugger;
   const { pair } = req.query || {};
+
   res.writeHead(301, {
     location: `bnc://app.binance.com/trade/trade?at=spot&symbol=${pair}`,
   });
   res.end();
-  //res.status(200).json({ pair });
 };
